b98c47f02cc29c4997c952771dfc185c3db5716f
sergycg <sergycg@gmail.com>
2022-03-09 17:10:20 +0100

    v2

--- src/main/java/com/oauth/security/AuthorizationServerConfig.java
+++ src/main/java/com/oauth/security/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.oauth.security;
 
 import java.util.Arrays;
+import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -71,7 +72,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey(env.getProperty("config.security.oauth.jwt.key"));
+//		tokenConverter.setSigningKey(env.getProperty("config.security.oauth.jwt.key"));
+		tokenConverter.setSigningKey(Base64.getEncoder().encodeToString(env.getProperty("config.security.oauth.jwt.key").getBytes()));
 		return tokenConverter;
 	}
 	
